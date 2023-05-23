0a4f0ac9604b925661f14adcf8eaf9f57b5a1dca
Imran Shahzad <imranshahzad786@gmail.com>
2023-05-23 11:51:20 +0500

    Javascript class 2

--- Js/index.js
+++ Js/index.js
@@ -1,19 +1,76 @@
-const title=document.getElementById('title')
-const bio=document.getElementById('bio')
+// const title=document.getElementById('title')
+// const bio=document.getElementById('bio')
 
 // title.innerHTML="Ali Akmal"
 
-title.style.backgroundColor="orange"
+// title.style.backgroundColor="orange"
 
-title.addEventListener('click',function(){
-title.classList.add('name')   
-})
+// title.addEventListener('click',function(){
+// title.classList.add('name')   
+// })
 
-bio.addEventListener('mouseover',function(){
-  bio.classList.add('bio')
-})
-bio.addEventListener('mouseout',function(){
-bio.classList.remove('bio')
+// bio.addEventListener('mouseover',function(){
+//   bio.classList.add('bio')
+// })
+// bio.addEventListener('mouseout',function(){
+// bio.classList.remove('bio')
+// })
 
-  
-})
+// function ushak(){
+//   // console.log("asdasdasds");
+// return 2+3;
+// }
+
+
+// console.log(ushak());
+// let 
+// const 
+// var
+function arslan(){
+ for(let j=1;j<=10;j++){
+
+
+  const para=document.createElement('h1')
+  para.innerHTML="Today is our Second Javascript Class. And we are learning basic concepts in JavaScript"
+  para.style.color="red"
+  document.body.appendChild(para)
+  }
+} 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Var can be redeclare and reinitilize 
+// var a=90;
+// a=100;
+// var a=500;
+// console.log(a);
+
+
+  // let x;
+  // x=09;
+  // const c=100;
+  // const d=900;
+  // const e=60;
+
+  // d=800;
+  // console.log(c,d);
